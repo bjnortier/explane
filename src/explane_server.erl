463d114f97c29262d771f29f09fa6ff4a8a42272
Benjamin Nortier <bjnortier@gmail.com>
2010-08-19 22:28:04 +0100

    Added listener support and value passing to listeners

--- src/explane_server.erl
+++ src/explane_server.erl
@@ -1,10 +1,14 @@
 -module(explane_server).
 -behaviour(gen_server).
--export([start_link/0, state/0]).
+-export([start_link/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
+-export([add_listener/1,
+         remove_listener/1]).
 
--record(state, {socket}).
+-include_lib("eunit/include/eunit.hrl").
+
+-record(state, {socket, listeners=[]}).
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
@@ -14,26 +18,38 @@ init([]) ->
     {ok, Socket} = gen_udp:open(Port, []),
     {ok, #state{socket = Socket}}.
 
-state() ->
-    gen_server:call(?MODULE, state).
+add_listener(ListenerPid) ->
+    gen_server:call(?MODULE, {add_listener, ListenerPid}).
+
+remove_listener(ListenerPid) ->
+    gen_server:call(?MODULE, {remove_listener, ListenerPid}).
 
 %% callbacks
-handle_call(state, _From, State) ->
-    io:format("server state: ~p~n", [State]),
-    {reply, ok, State};
+handle_call({add_listener, ListenerPid}, _From, State) ->
+    Listeners = State#state.listeners,
+    NewListeners = [ListenerPid|Listeners],
+    NewState = State#state{listeners=NewListeners},
+    {reply, ok, NewState};
+handle_call({remove_listener, ListenerPid}, _From, State) ->
+    Listeners = State#state.listeners,
+    NewListeners = lists:delete(ListenerPid, Listeners),
+    NewState = State#state{listeners=NewListeners},
+    {reply, ok, NewState}.
 
-handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
 %% In active mode (the default for a gen_udp socket), incoming packets
 %% are received as messages
-handle_info({udp, _Socket, FromIP, FromPort, Packet}, State) ->
+handle_info({udp, _Socket, _FromIP, _FromPort, Packet}, State) ->
     Parsed = explane_packet:parse(Packet),
-    io:format("~p~n", [Parsed]),
+    Values = packet_to_values(Parsed),
+    Listeners = State#state.listeners,
+    lists:map(fun(Listener) ->
+                      Listener ! Values
+              end,
+              Listeners),
     {noreply, State};
 %% Discard other messages
 handle_info(_Info, State) ->
@@ -44,6 +60,52 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+
+-ifdef(TEST).
+packet_to_values_test() ->
+    [
+     ?assertMatch(
+        [
+         {altitude, -1.3111454248428345},
+         {roll, 2.7012016773223877},
+         {pitch, -0.8095226883888245},
+         {speed, 5.157218933105469}],
+        packet_to_values(
+          {"DATA4",
+           [{3,
+             [5.157218933105469,6.044749736785889,6.047330856323242,7.419618129730225,
+              -999.0,5.934821605682373,6.959144115447998,8.538344383239746]},
+            {18,
+             [-0.8095226883888245,2.7012016773223877,347.249755859375,350.6871032714844,
+              350.0473327636719,-999.0,-999.0,3.437335729598999]},
+            {20,
+             [50.78217315673828,-0.44528934359550476,-1.3111454248428345,
+              -4.337009429931641,0.0,-1.3572769165039063,50.0,-2.0]}]}))
+       ].
+-endif.
+
+
+packet_to_values({"DATA4", Rows}) ->
+    packet_to_values(Rows, []).
+
+packet_to_values([{3, [A,_,_,_,_,_,_,_]}|Rest], Acc) ->
+    Acc1 = [{speed, A}|Acc],
+    packet_to_values(Rest, Acc1);
+packet_to_values([{18, [A,B,_,_,_,_,_,_]}|Rest], Acc) ->
+    Acc1  = [{pitch, A}|Acc],
+    Acc2 = [{roll, B}|Acc1],
+    packet_to_values(Rest, Acc2);
+packet_to_values([{20, [_,_,C,_,_,_,_,_]}|Rest], Acc) ->
+    Acc1 = [{altitude, C}|Acc],
+    packet_to_values(Rest, Acc1);
+packet_to_values([_|Rest], Acc) ->
+    packet_to_values(Rest, Acc);
+packet_to_values([], Acc) ->
+    Acc.
+    
+                        
+
 
 
 
